dc8f3040aa04e6425a07ee1a3aa3480b2be9c401
Inah Choi <jody980502@gmail.com>
2022-01-18 23:29:22 +0900

    큰 수의 법칙

---
+++ src/greedy/GreedyPractice2.java
@@ -0,0 +1,28 @@
+package greedy;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class GreedyPractice2 {
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int m = scan.nextInt();
+		int k = scan.nextInt();
+		int[] arr = new int[n];
+		int rslt = 0;
+		
+		for(int i = 0; i < n; i++) arr[i] = scan.nextInt();
+		
+		Arrays.sort(arr);
+		
+		int first = arr[arr.length - 1];
+		int second = arr[arr.length - 2];
+		
+		rslt = (first * k + second) * (m / (k + 1));
+		
+		if(m % (k + 1) != 0) rslt += (m % (k + 1)) * first;
+		
+		System.out.println(rslt);
+	}
+}
